8663eda7704528456992225894ec8fc590f77ea8
BekturMuratov <muratov225@gmail.com>
2023-12-28 14:31:47 +0600

    working!

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path'
 
 @Module({
-  imports: [BlogModule],
+  imports: [BlogModule,
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'uploads'),
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
